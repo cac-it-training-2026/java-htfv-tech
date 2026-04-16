5f1170cdc368baad9f33d0c73d3afe038f159982
htakaha <htakaha@C010423346.zipangu.cac.co.jp>
2026-04-16 14:15:59 +0900

    Question7-5完了

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_5.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_5.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_5 {
 
@@ -14,5 +16,24 @@ public class Question07_5 {
 		int[] amounts = new int[5];
 
 		// TODO: 実装ここから
+		int sum = 0;
+		double discount = 0.9;
+		double tax = 1.1;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.print(itemNames[i] + "の購入数:>");
+			String str = reader.readLine();
+			amounts[i] = Integer.parseInt(str);
+			sum += (int) (amounts[i] * prices[i] * tax);
+		}
+		System.out.println("購入内容一覧");
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.println(
+					itemNames[i] + ":単価" + prices[i] + "円(税込" + (int) (prices[i] * tax) + "円)×" + amounts[i] + "個＝"
+							+ (int) (amounts[i] * prices[i] * tax) + "円");
+		}
+		System.out.println("合計(税込)：" + sum + "円");
+		sum *= discount;
+		System.out.println("割引適用後合計（税込）:" + sum + "円");
 	}
 }
