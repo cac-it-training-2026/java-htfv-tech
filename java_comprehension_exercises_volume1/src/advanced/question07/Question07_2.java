0cc875cf5d2f4cca81d80c1cb12bf8a6959dcffc
htakaha <htakaha@C010423346.zipangu.cac.co.jp>
2026-04-16 12:16:47 +0900

    Question7-2完了

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_2.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_2.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_2 {
 
@@ -9,9 +11,25 @@ public class Question07_2 {
 		// 登録済み商品（値は変更可）
 		String[] itemNames = { "ノート", "ボールペン", "消しゴム", "はさみ", "のり" };
 		// 単価（値は変更可）
-		int[] prices = { 100, 200, 300, 400, 500 };
+		int[] prices = { 120, 80, 550, 500, 100 };
 
 		// TODO: 実装ここから
-		
+		int[] amounts;
+		int sum = 0;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		amounts = new int[itemNames.length];
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.print(itemNames[i] + "の購入数:>");
+			String str = reader.readLine();
+			amounts[i] = Integer.parseInt(str);
+			sum += amounts[i] * prices[i];
+		}
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.println(itemNames[i] + "：" + prices[i] * amounts[i] + "円");
+		}
+		System.out.println("合計金額：" + sum + "円");
+
 	}
 }
